3777a7a98c942b8010266085d9b28493432b566c
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2024-06-21 05:34:57 +0800

    style #11 add password input toggle

--- src/lib/components/SignUp.tsx
+++ src/lib/components/SignUp.tsx
@@ -3,11 +3,16 @@
 import { signup } from "@actions/signup";
 import { useFormState } from "react-dom";
 import { Input } from "@nextui-org/react";
+import { useState } from "react";
+import { EyeFilledIcon } from "./icons/EyeFilledIcon";
+import { EyeSlashFilledIcon } from "./icons/EyeSlashFilledIcon";
 const initialState = { status: 0, message: "" };
 
 export default function SignUp() {
     const [state, formAction] = useFormState(signup, initialState);
+    const [isVisible, setIsVisible] = useState(false);
 
+    const toggleVisibility = () => setIsVisible(!isVisible);
     console.log(state.message);
     return (
         <div className="space-y-4 h-screen flex justify-center items-center">
@@ -45,15 +50,28 @@ export default function SignUp() {
                     description="Enter your Email Address"
                     isClearable={true}
                 />
+
                 <Input
                     className="max-w-sm"
                     label="Password"
                     labelPlacement="inside"
                     isRequired
                     name="password"
-                    type="password"
+                    type={isVisible ? "text" : "password"}
                     description="Enter your Password"
-                    isClearable={true}
+                    endContent={
+                        <button
+                            className="focus:outline-none"
+                            type="button"
+                            onClick={toggleVisibility}
+                        >
+                            {isVisible ? (
+                                <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                            ) : (
+                                <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                            )}
+                        </button>
+                    }
                 />
                 <Input
                     role="button"
