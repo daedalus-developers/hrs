d551b40f51dfc89c9d675b4ae37ca85506d44d64
Denver James Duran <128191960+DDuran19@users.noreply.github.com>
2024-06-21 21:36:20 +0800

    feat: add styles to auth related pages (#13)
    * Include components inside lib/components to tailwind
    
    * style #11 Add some basic styles
    
    * use type  email
    
    * style #11 add password input toggle
    
    * style #11 Create basic style for /login
    
    * #11 Create temporary icons

--- src/lib/components/SignUp.tsx
+++ src/lib/components/SignUp.tsx
@@ -2,24 +2,91 @@
 
 import { signup } from "@actions/signup";
 import { useFormState } from "react-dom";
-
+import { Input } from "@nextui-org/react";
+import { useState } from "react";
+import { EyeFilledIcon } from "./icons/EyeFilledIcon";
+import { EyeSlashFilledIcon } from "./icons/EyeSlashFilledIcon";
 const initialState = { status: 0, message: "" };
 
 export default function SignUp() {
-  const [state, formAction] = useFormState(signup, initialState);
+    const [state, formAction] = useFormState(signup, initialState);
+    const [isVisible, setIsVisible] = useState(false);
 
-  console.log(state.message);
-  return (
-    <div>
-      <form action={formAction} className="text-black">
-        <input name="lastName" type="text" placeholder="Last Name" />
-        <input name="firstName" type="text" placeholder="First Name" />
-        <input name="email" type="text" placeholder="Email Address" />
-        <input name="password" type="password" placeholder="Password" />
-        <button type="submit" className="bg-white">
-          Submit
-        </button>
-      </form>
-    </div>
-  );
+    const toggleVisibility = () => setIsVisible(!isVisible);
+    console.log(state.message);
+    return (
+        <div className="space-y-4 h-screen flex justify-center items-center">
+            <form
+                action={formAction}
+                className="container flex flex-col justify-center items-center space-y-4 mx-auto"
+            >
+                <Input
+                    className="max-w-sm"
+                    label="First Name"
+                    labelPlacement="inside"
+                    isRequired
+                    name="firstName"
+                    type="text"
+                    description="Enter your First Name"
+                    isClearable={true}
+                />
+                <Input
+                    className="max-w-sm"
+                    label="Last Name"
+                    labelPlacement="inside"
+                    isRequired
+                    name="lastName"
+                    type="text"
+                    description="Enter your Last Name"
+                    isClearable={true}
+                />
+                <Input
+                    className="max-w-sm"
+                    label="Email Address"
+                    labelPlacement="inside"
+                    isRequired
+                    name="email"
+                    type="email"
+                    description="Enter your Email Address"
+                    isClearable={true}
+                />
+
+                <Input
+                    className="max-w-sm"
+                    label="Password"
+                    labelPlacement="inside"
+                    isRequired
+                    name="password"
+                    type={isVisible ? "text" : "password"}
+                    description="Enter your Password"
+                    endContent={
+                        <button
+                            className="focus:outline-none"
+                            type="button"
+                            onClick={toggleVisibility}
+                        >
+                            {isVisible ? (
+                                <EyeSlashFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                            ) : (
+                                <EyeFilledIcon className="text-2xl text-default-400 pointer-events-none" />
+                            )}
+                        </button>
+                    }
+                />
+                <Input
+                    role="button"
+                    className="max-w-sm"
+                    type="submit"
+                    value={"Sign Up"}
+                />
+                <span>
+                    Already have an account?{" "}
+                    <a href="/login" className="text-blue-500">
+                        Login
+                    </a>
+                </span>
+                <p className="text-red-500">{state.message || ""}</p>
+            </form>
+        </div>
+    );
 }
