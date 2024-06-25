bd1c0951d8f992b335c4a2773e5a32c74201bc51
Artifex <mac@mkra.dev>
2024-06-26 03:01:45 +0800

    chore: bump and fix packages

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from 'drizzle-kit';
-import 'dotenv/config';
+import { defineConfig } from "drizzle-kit";
+import "dotenv/config";
 
 export default defineConfig({
-  schema: ['./src/lib/server/schemas/'],
-  dialect: 'postgresql',
-  out: './migrations',
+  schema: ["./src/lib/server/schemas/"],
+  dialect: "postgresql",
+  out: "./migrations",
   dbCredentials: {
-    url: process.env.DB_URL!
+    url: process.env.DB_URL!,
   },
   verbose: true,
-})
-
+});
