53480a17dd771ee90dd8b7e09a72e6c17ed0acd9
Artifex <mac@mkra.dev>
2024-06-28 02:48:25 +0800

    hook error

--- src/lib/server/routes/auth/signup.api.ts
+++ src/lib/server/routes/auth/signup.api.ts
@@ -1,81 +1,89 @@
-import { OpenAPIHono, createRoute } from "@hono/zod-openapi";
-import { lucia } from "@server/auth";
-import { sendEmailVerificationCode } from "@server/mailer";
-import { insertUser } from "@server/mutations";
-import { queryEmail } from "@server/queries";
-import { responseSchema } from "@server/schemas";
-import { signupSchema } from "@server/schemas/auth";
-import { generateEmailVerificationCode } from "@server/services/verification";
-import { ContextVars } from "@types";
-import { Argon2id } from "@utils/argon2";
-import { generateIdFromEntropySize } from "lucia";
-import { authMiddleware } from "../auth.middleware";
+import { OpenAPIHono, createRoute } from '@hono/zod-openapi';
+import { lucia } from '@server/auth';
+import { sendEmailVerificationCode } from '@server/mailer';
+import { insertUser } from '@server/mutations';
+import { queryEmail } from '@server/queries';
+import { responseSchema } from '@server/schemas';
+import { SignupSchema, signupSchema } from '@server/schemas/auth';
+import { generateEmailVerificationCode } from '@server/services/verification';
+import { ContextVars } from '@types';
+import { Argon2id } from '@utils/argon2';
+import { generateIdFromEntropySize } from 'lucia';
+import { authMiddleware } from '../auth.middleware';
+import { processZodError } from '@utils/error.util';
 
 const signupSpec = createRoute({
-  method: 'post',
-  path: 'signup',
-  tags: ['Auth'],
-  summary: 'Register user',
-  middleware: [authMiddleware],
-  request: {
-    body: {
-      description: 'Request body',
-      content: {
-        "application/json": { schema: signupSchema.openapi('Signup') },
-      },
-      required: true,
-    },
-  },
-  responses: {
-    200: {
-      content: {
-        "application/json": { schema: responseSchema },
-      },
-      description: 'Success',
-    },
-    400: {
-      content: {
-        "application/json": { schema: responseSchema },
-      },
-      description: 'Bad request',
-    },
-  },
-})
+	method: 'post',
+	path: 'signup',
+	tags: ['Auth'],
+	summary: 'Register user',
+	middleware: [authMiddleware],
+	request: {
+		body: {
+			description: 'Request body',
+			content: {
+				'application/json': { schema: signupSchema.openapi('Signup') }
+			},
+			required: true
+		}
+	},
+	responses: {
+		200: {
+			content: {
+				'application/json': { schema: responseSchema }
+			},
+			description: 'Success'
+		},
+		400: {
+			content: {
+				'application/json': { schema: responseSchema }
+			},
+			description: 'Bad request'
+		}
+	}
+});
 
-const signup = new OpenAPIHono<{ Variables: ContextVars }>().openapi(signupSpec, async (c) => {
-  const { lastName, firstName, email, password } = c.req.valid('json');
+const signup = new OpenAPIHono<{ Variables: ContextVars }>({
+	defaultHook: (result, c) => {
+		if (!result.success) {
+			const response = processZodError<SignupSchema>('Invalid form', result.error);
+			return c.json(response, 422);
+		}
+	}
+}).openapi(signupSpec, async (c) => {
+	const { lastName, firstName, email, password } = c.req.valid('json');
 
-  const result = await queryEmail.execute({ email: email });
-  if (result?.length > 0) return c.json({ message: "Email already taken" }, 400)
+	const result = await queryEmail.execute({ email: email });
+	if (result?.length > 0) return c.json({ message: 'Email already taken' }, 400);
 
-  const userId = generateIdFromEntropySize(10);
+	const userId = generateIdFromEntropySize(10);
 
-  const passwordHash = await new Argon2id().hash(password);
+	const passwordHash = await new Argon2id().hash(password);
 
-  const user = {
-    id: userId,
-    lastName: lastName,
-    firstName: firstName,
-    email: email,
-    emailVerified: false,
-    password: passwordHash,
-  };
+	const user = {
+		id: userId,
+		lastName: lastName,
+		firstName: firstName,
+		email: email,
+		emailVerified: false,
+		password: passwordHash
+	};
 
-  await insertUser(user);
+	await insertUser(user);
 
-  const session = await lucia.createSession(userId, {});
-  const sessionCookie = lucia.createSessionCookie(session.id);
+	const session = await lucia.createSession(userId, {});
+	const sessionCookie = lucia.createSessionCookie(session.id);
 
-  c.header('Set-Cookie', sessionCookie.serialize(), { append: true });
+	c.header('Set-Cookie', sessionCookie.serialize(), { append: true });
 
-  const verificationCode = await generateEmailVerificationCode(userId, email);
-  await sendEmailVerificationCode(
-    email,
-    verificationCode.code!,
-    verificationCode.expiresAt!.toString(),
-  );
+	const verificationCode = await generateEmailVerificationCode(userId, email);
+	await sendEmailVerificationCode(
+		email,
+		verificationCode.code!,
+		verificationCode.expiresAt!.toString()
+	);
 
-  return c.json({ message: "Signup sucessful." })
-})
+	return c.json({ message: 'Signup sucessful.' });
+});
 
-export { signup }
+export { signup };
--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,2 +1,29 @@
-export * from "./user.type"
-export * from "./variables.type"
+export * from './user.type';
+export * from './variables.type';
+
+/**
+ * @description A Record Representation of Field Errors
+ */
+export type FieldErrors = Record<string, string[]>;
+
+/**
+ * @description Mapped Field Errors Maps it to the original key's of the schema
+ */
+export type MappedFieldErrors<T = FieldErrors> = {
+	[K in keyof T]: string[];
+};
+
+/**
+ * @description The Response returned when the form is invalid
+ */
+export type FormErrorResponse<T = FieldErrors> = {
+	message: string;
+	fields: MappedFieldErrors<Partial<T>>;
+};
+
+export type ApiResponse<T> = {
+	data: T | null;
+	errors: FormErrorResponse | string | null;
+	status: number;
+	message: string | null;
+};
